86f7b3dbe1c6794c67daa61caea526c57b300133
Zeke Chan <zeke.chan.tk@gmail.com>
2021-12-23 22:24:00 +0800

    Added countdown

--- Completed/main/data.lua
+++ Completed/main/data.lua
@@ -1,4 +1,71 @@
--- Put functions in this file to use them in several other scripts.
--- To get access to the functions, you need to put:
--- require "my_directory.my_file"
--- in any script using the functions.
+local M = {}
+
+M.STATE_MENU = 1
+M.STATE_CONTROLS = 2
+M.STATE_READY = 3
+M.STATE_PLAY = 4
+M.STATE_PAUSE = 5
+M.STATE_GAMEOVER = 6
+
+M.state = M.STATE_MENU
+M.score_player1 = 0
+M.score_player2 = 0
+M.speed = M.STARTING_SPEED
+M.distance = 0
+M.sections = {0, 0, 0}
+M.state = M.STATE_WAIT
+M.time_elapsed = 0
+
+function M.updateScore(score, player)
+	M.score = M.score + math.floor(score)
+end
+
+function M.getScore()
+	return M.score
+end
+
+function M.startGame()
+	M.level = M.STARTING_LEVEL
+	M.score = 0
+	M.time_elapsed = 0
+	M.speed = M.STARTING_SPEED
+	M.sections = {'', '', ''}
+	M.distance = 0
+	M.state = M.STATE_WAIT
+end
+
+function M.update_time_elapsed(time)
+	M.time_elapsed = M.time_elapsed + time
+	return M.time_elapsed
+end
+
+function M.reset_level_duration()
+	M.time_elapsed = M.time_elapsed - M.get_level_duration()
+end
+
+function M.get_level_duration()
+	return M.LEVEL_DURATION[M.level]
+end
+
+function M.next_level()
+	if M.level < M.MAX_LEVELS then
+		M.level = M.level + 1
+		M.speed = M.speed + M.SPEED_INCREMENT
+	end
+
+	return M.current_level()
+end
+
+function M.current_level()
+	return M.level
+end
+
+function M.get_state()
+	return M.state
+end
+
+function M.set_state(state)
+	M.state = state
+end
+
+return M
--- Completed/main/messages.lua
+++ Completed/main/messages.lua
@@ -1,4 +1,25 @@
--- Put functions in this file to use them in several other scripts.
--- To get access to the functions, you need to put:
--- require "my_directory.my_file"
--- in any script using the functions.
+local M = {}
+
+M.UPDATE_SCORE = hash("update_score")
+M.COLLECT_ORB = hash("collect_orb")
+M.INPUT_JUMP = hash("jump")
+M.INPUT_FALL = hash("fall")
+M.INPUT_UP = hash("up")
+M.INPUT_DOWN = hash("down")
+M.INPUT_GRAVIY = hash("gravity")
+M.PROXY_LOADED = hash("proxy_loaded")
+M.GAME_OVER = hash("game_over")
+M.LAYOUT_CHANGED = hash("layout_changed")
+M.MODE_LANDSCAPE = hash("Landscape")
+M.MODE_PORTRAIT = hash("Portrait")
+M.WINDOW_RESIZED = hash("window_resized")
+M.UPDATE_WINDOW = hash("update_window")
+M.NEXT_LEVEL = hash("next_level")
+
+M.SHOW_GAME = hash("show_game")
+M.SHOW_MENU = hash("show_menu")
+
+M.REGISTER = hash("register")
+M.UNREGISTER = hash("unregister")
+
+return M
