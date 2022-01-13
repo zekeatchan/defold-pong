92e56808a5d5f3152ebaa8a536ae4ef5d090b0ba
Zeke Chan <zeke.chan.tk@gmail.com>
2022-01-13 13:09:29 +0800

    Added ball collisions

--- Completed/main/data.lua
+++ Completed/main/data.lua
@@ -1,20 +1,13 @@
 local M = {}
 
-M.STATE_MENU = 1
-M.STATE_CONTROLS = 2
-M.STATE_READY = 3
-M.STATE_PLAY = 4
-M.STATE_PAUSE = 5
-M.STATE_GAMEOVER = 6
+M.STATE_COUNTDOWN = 1
+M.STATE_PLAY = 2
+M.STATE_PAUSE = 3
+M.STATE_GAMEOVER = 4
 
-M.state = M.STATE_MENU
+M.state = M.STATE_COUNTDOWN
 M.score_player1 = 0
 M.score_player2 = 0
-M.speed = M.STARTING_SPEED
-M.distance = 0
-M.sections = {0, 0, 0}
-M.state = M.STATE_WAIT
-M.time_elapsed = 0
 M.has_touch_screen = false
 M.players = 1
 
@@ -26,40 +19,26 @@ function M.getScore()
 	return M.score
 end
 
-function M.startGame()
-	M.level = M.STARTING_LEVEL
-	M.score = 0
-	M.time_elapsed = 0
-	M.speed = M.STARTING_SPEED
-	M.sections = {'', '', ''}
-	M.distance = 0
+function M.newGame()
+	M.state = M.STATE_COUNTDOWN
+	M.score_player1 = 0
+	M.score_player2 = 0
 	M.state = M.STATE_WAIT
+	M.has_touch_screen = false
+	M.players = 1
 end
 
-function M.update_time_elapsed(time)
-	M.time_elapsed = M.time_elapsed + time
-	return M.time_elapsed
+function M.playGame()
+	M.state = M.STATE_PLAY
 end
 
-function M.reset_level_duration()
-	M.time_elapsed = M.time_elapsed - M.get_level_duration()
-end
+function M.player_scored(player)
+	print(player)
+	M.state = M.STATE_PAUSE
 
-function M.get_level_duration()
-	return M.LEVEL_DURATION[M.level]
-end
-
-function M.next_level()
-	if M.level < M.MAX_LEVELS then
-		M.level = M.level + 1
-		M.speed = M.speed + M.SPEED_INCREMENT
+	if player == 1 then M.score_player1 = M.score_player1 + 1
+	elseif player == 2 then M.score_player2 = M.score_player2 + 1
 	end
-
-	return M.current_level()
-end
-
-function M.current_level()
-	return M.level
 end
 
 function M.get_state()
--- Completed/main/messages.lua
+++ Completed/main/messages.lua
@@ -1,20 +1,19 @@
 local M = {}
 
-M.UPDATE_SCORE = hash("update_score")
-M.COLLECT_ORB = hash("collect_orb")
-M.INPUT_JUMP = hash("jump")
-M.INPUT_FALL = hash("fall")
-M.INPUT_UP = hash("up")
-M.INPUT_DOWN = hash("down")
-M.INPUT_GRAVIY = hash("gravity")
+M.GOAL_1 = hash("goal_1")
+M.GOAL_2 = hash("goal_2")
+M.PADDLE = hash("paddle")
+M.CONTACT_POINT = hash("contact_point_response")
+
 M.PROXY_LOADED = hash("proxy_loaded")
 M.GAME_OVER = hash("game_over")
+
 M.LAYOUT_CHANGED = hash("layout_changed")
 M.MODE_LANDSCAPE = hash("Landscape")
 M.MODE_PORTRAIT = hash("Portrait")
 M.WINDOW_RESIZED = hash("window_resized")
+
 M.UPDATE_WINDOW = hash("update_window")
-M.NEXT_LEVEL = hash("next_level")
 
 M.SHOW_GAME = hash("show_game")
 M.SHOW_MENU = hash("show_menu")
